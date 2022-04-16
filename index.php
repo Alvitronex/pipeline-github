4fedda060d1a835cde1e62b3e9bbc0118585a350
Alvitronex <82683379+Alvitronex@users.noreply.github.com>
2022-04-16 11:02:01 -0600

    init

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ echo '<h1>This code its doing and
         PHP code</h1>';
     ?>
     <h1>We've deployed this app for look at structure and if you like to this, GREAT!!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus harum quo recusandae suscipit adipisci. Nostrum, vitae. Repellendus earum, sequi veniam soluta harum exercitationem amet esse, tempore pariatur quia nostrum blanditiis.</p>
     
 </body>
 </html>
