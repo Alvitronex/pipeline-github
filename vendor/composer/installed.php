5e613602cc0e9b3eeb4b792257c5918001024234
Alvitronex <82683379+Alvitronex@users.noreply.github.com>
2022-04-16 11:18:31 -0600

    text

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e9881ae90c2b53e9baf4f648df73103e35610b12',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e9881ae90c2b53e9baf4f648df73103e35610b12',
+            'dev_requirement' => false,
+        ),
+    ),
+);
