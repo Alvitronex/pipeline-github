8cd0cd2a0058c6ecd094858024591649fb53e53b
Alvitronex <82683379+Alvitronex@users.noreply.github.com>
2022-04-16 10:48:06 -0600

    init

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deploy App PHP </title>
+</head>
+<body>
+    <?php
+echo '<h1>This code its doing and
+        PHP code</h1>';
+    ?>
+    <h1>We've deployed this app for look at structure and if you like to this, GREAT!!</h1>
+    
+</body>
+</html>
